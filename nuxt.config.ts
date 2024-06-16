e9ba1403ca6d9089b7d1de38d634e390cb0a849d
dvd19146042 <dvd19146042@gmail.com>
2024-06-16 13:48:56 +0700

    update edge

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui','nuxt-server-utils'],
+  nitro: {
+    preset: 'vercel-edge',
+  },
   runtimeConfig:{
    public:{
     mongodbUri:"mongodb+srv://iuhproject:12345Iuh@iuh.vkrnst0.mongodb.net/database",
