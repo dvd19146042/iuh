5d80ec94e12543b40077cc083d4b580283736b7e
dvd19146042 <dvd19146042@gmail.com>
2024-06-16 15:24:05 +0700

    remove

--- .vercel/output/functions/__nitro.func/chunks/_/temp.model.mjs
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const TempSchema = new Schema({
-  code: String
-});
-const Temp = model("Temp", TempSchema);
-
-export { Temp as T };
-//# sourceMappingURL=temp.model.mjs.map
--- .vercel/output/functions/__nitro.func/index.mjs
+++
@@ -1,7 +0,0 @@
-import process from 'node:process';globalThis._importMeta_={url:import.meta.url,env:process.env};export { J as default } from './chunks/runtime.mjs';
-import 'node:http';
-import 'node:https';
-import 'fs';
-import 'path';
-import 'mongoose';
-//# sourceMappingURL=index.mjs.map
